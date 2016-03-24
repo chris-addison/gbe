2622c0b768611857070a3f1d7de6dfb78e28dbe3
David Addison <addy@Davids-Old-iMac.local>
2016-03-23 22:23:36 -0700

    add a emacs header for tab length

--- debug.c
+++ debug.c
@@ -1,3 +1,4 @@
+/* -*-mode:c; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
 #include "main.h"
 
 //simple gdb-like debug
--- main.h
+++ main.h
@@ -1,3 +1,4 @@
+/* -*-mode:c; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
 #ifndef MAIN_H
 #define MAIN_H
 
